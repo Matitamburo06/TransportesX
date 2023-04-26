e4af9df1f7cbc46ba325521a8997edbac9a14f20
Matias Tamburo <matitamburo68@gmail.com>
2023-04-26 18:54:54 -0300

    3er commit

---
+++ Front End/js/Transport.js
@@ -0,0 +1,130 @@
+// confirm("Esta es la pagina de Transportes X")
+
+// hola soy un comentario multilinea
+
+// confirm("Hola 23067!!")
+
+//diferencias entre var y let
+
+var nombre ="Matias"
+
+console.log(nombre)
+
+
+// if(
+//     true
+// ){
+//     let edad= 23;
+//     console.log(edad)
+// }
+
+// console.log(nombre) //output
+
+
+
+// var num1 =23;
+
+
+// function mostranum(){
+//     var num1=23
+// }
+
+// console.log(num1)
+
+
+
+const PI= 3.14
+
+console.log(PI)
+
+
+// function greetUser() {
+// 	var userName = prompt("Please enter your name");
+// 	alert("Hello, " + userName + "!");
+// }
+
+// // This code runs the greetUser function when the page loads
+// window.onload = greetUser;
+
+
+// // Prompt the user to enter their name
+// var name = prompt("What is your name?");
+
+// // Display a personalized greeting
+// alert("Hello, " + name + "! Welcome to my website.");
+
+
+
+// // Define an array of numbers
+// var numbers = [1, 2, 3, 4, 5];
+
+// // Access the first element of the array
+// var firstNumber = numbers[0];
+
+// // Access the last element of the array
+// var lastNumber = numbers[numbers.length - 1];
+
+// // Loop through the array and print each number
+// for (var i = 0; i < numbers.length; i++) {
+//     console.log(numbers[i]);
+// }
+
+
+
+
+// var fruits = ['apple', 'banana', 'orange'];
+
+// console.log(fruits)
+
+
+// // var firstFruit = fruits[0]; // 'apple'
+// // var secondFruit = fruits[1]; // 'banana'
+// // var lastFruit = fruits[fruits.length - 1]; // 'orange'
+
+// // fruits[1] = 'pear'; // changes 'banana' to 'pear'
+
+// // fruits.push('grape'); // adds 'grape' to the end of the array
+// // fruits.unshift('kiwi'); // adds 'kiwi' to the beginning of the array
+
+
+// // var lastFruit = fruits.pop(); // removes 'orange' from the end of the array and returns it
+// // var firstFruit = fruits.shift(); // removes 'kiwi' from the beginning of the array and returns it
+
+// // Using a for loop
+// for (var i = 0; i < fruits.length; i++) {
+//     console.log(fruits[i]);
+// }
+
+// // Using the forEach method
+// fruits.forEach(function(fruit) {
+//     console.log(fruit);
+// });
+
+
+
+// function greet() {
+//     console.log("Hello, world!");
+//   }
+  
+//   setTimeout(greet, 3000); // calls greet() function after 3 seconds (3000 milliseconds)
+
+
+//   setTimeout(function() {
+//     console.log("This message will be displayed after 2 seconds.");
+//   }, 2000);
+
+
+// function doSomething(callback) {
+//     console.log("Doing something...");
+//     setTimeout(function() {
+//       console.log("Finished doing something.");
+//       callback();
+//     }, 3000);
+//   }
+  
+//   function sayGoodbye() {
+//     console.log("Goodbye!");
+//   }
+  
+//   doSomething(sayGoodbye);
+
